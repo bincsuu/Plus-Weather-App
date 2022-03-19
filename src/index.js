8e65126eeb6f24096edb424b342a021f7905048d
bincsuu <bianca.sero01@gmail.com>
2022-03-19 19:35:30 +0100

    Added units
    Added wind and humidity units

--- src/index.js
+++ src/index.js
@@ -41,12 +41,12 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   icon.setAttribute("alt", response.data.weather[0].description);
+  console.log(response.data);
 }
 
 function citySearch(city) {
   let apiKey = "fdd9ef011491bdd0ac653f81ffb9ed48";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayTemperature);
 }
 
